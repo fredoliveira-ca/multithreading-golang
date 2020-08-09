8259899b90fd34eb275a892e5fbd090be62dcc7d
Fred Oliveira <fredoliveira.ca@gmail.com>
2020-08-09 19:40:32 -0300

    Calculating the separation

--- boids/boid.go
+++ boids/boid.go
@@ -12,21 +12,6 @@ type Boid struct {
 	velocity Vector2D
 	id       int
 }
-
-func (b *Boid) moveOne() {
-	b.velocity = b.velocity.Add(b.calculateAcceleration()).limit(-1, 1)
-	boidMap[int(b.position.x)][int(b.position.y)] = -1
-	b.position = b.position.Add(b.velocity)
-	boidMap[int(b.position.x)][int(b.position.y)] = b.id
-	next := b.position.Add(b.velocity)
-	if next.x > screenWidth || next.x < 0 {
-		b.velocity = Vector2D{-b.velocity.x, b.position.y}
-	}
-	if next.y > screenHeight || next.y < 0 {
-		b.velocity = Vector2D{b.velocity.x, b.position.y}
-	}
-}
-
 func (b *Boid) start() {
 	for {
 		b.moveOne()
@@ -36,23 +21,31 @@ func (b *Boid) start() {
 
 func (b *Boid) calculateAcceleration() Vector2D {
 	upper, lower := b.position.AddV(viewRadius), b.position.AddV(-viewRadius)
-	averageVelocity := Vector2D{0,0}
+	averagePosition, averageVelocity, separation := Vector2D{0,0}, Vector2D{0,0}, Vector2D{0,0}
 	count := 0.0
 
+	rWlock.RLock()
 	for i := math.Max(lower.x, 0); i <= math.Min(upper.x, screenWidth); i++ {
 		for j := math.Max(lower.y, 0); j <= math.Min(upper.y, screenHeight); j++ {
 			if otherBoidId := boidMap[int(i)][int(j)]; otherBoidId != -1 && otherBoidId != b.id {
 				if dist := boids[otherBoidId].position.Distance(b.position); dist < viewRadius {
 					count++
 					averageVelocity = averageVelocity.Add(boids[otherBoidId].velocity)
+					averagePosition = averageVelocity.Add(boids[otherBoidId].position)
+					separation = separation.Add(b.position.Subtraction(boids[otherBoidId].position).DivisionV(dist))
 				}
 			}
 		}
 	}
+	rWlock.RUnlock()
+
 	accel := Vector2D{0, 0}
 	if count > 0 {
-		averageVelocity = averageVelocity.DivisionV(count)
-		accel = averageVelocity.Subtraction(b.velocity).MultiplyV(adjRate)
+		averagePosition, averageVelocity = averagePosition.DivisionV(count), averageVelocity.DivisionV(count)
+		accelAlignment := averageVelocity.Subtraction(b.velocity).MultiplyV(adjRate)
+		accelCohesion := averagePosition.Subtraction(b.position).MultiplyV(adjRate)
+		accelSeparation := separation.MultiplyV(adjRate)
+		accel = accel.Add(accelAlignment).Add(accelCohesion).Add(accelSeparation)
 	}
 	return accel
 }
@@ -66,4 +59,22 @@ func createBoid(bid int) {
 	boids[bid] = &b
 	boidMap[int(b.position.x)][int(b.position.y)] = b.id
 	go b.start()
+}
+
+
+func (b *Boid) moveOne() {
+	acceleration := b.calculateAcceleration()
+	rWlock.Lock()
+	b.velocity = b.velocity.Add(acceleration).limit(-1, 1)
+	boidMap[int(b.position.x)][int(b.position.y)] = -1
+	b.position = b.position.Add(b.velocity)
+	boidMap[int(b.position.x)][int(b.position.y)] = b.id
+	next := b.position.Add(b.velocity)
+	if next.x > screenWidth || next.x < 0 {
+		b.velocity = Vector2D{-b.velocity.x, b.position.y}
+	}
+	if next.y > screenHeight || next.y < 0 {
+		b.velocity = Vector2D{b.velocity.x, b.position.y}
+	}
+	rWlock.Unlock()
 }
